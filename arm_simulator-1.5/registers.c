e28cd9710ff02c72407f16afc312d6822cc7ad37
amine-elouechrine <mohamed.el-ouechrine@etu.univ-grenoble-alpes.fr>
2023-12-16 10:53:25 +0100

    origin

--- arm_simulator-1.5/registers.c
+++ arm_simulator-1.5/registers.c
@@ -25,70 +25,171 @@ Contact: Guillaume.Huard@imag.fr
 #include <stdlib.h>
 
 
-
 struct registers_data {
-    couple *tab;
+    uint32_t general_registers[16];
+    uint32_t cpsr;
+    uint32_t spsr[5];  // SPSR pour chaque mode {FIQ , IRQ , SVC , ABT , UND }
+    uint8_t current_mode;
 };
 
 
-//q bit
-
-
 registers registers_create() {
-    registers r = NULL;
-    /* � compl�ter... */
+    registers r = (registers)malloc(sizeof(struct registers_data));
+    if (r != NULL) {
+        // Initialiser les registres generaux
+        for (int i = 0; i < 16; i++) {
+            r->general_registers[i] = 0;
+        }
+
+        // Initialiser le CPSR et le mode par defaut (SVC)  : pour faire passer les testes (USR ne passe pas le teste )
+        r->cpsr = 0;
+        r->current_mode = SVC;
+
+        // Initialiser les SPSR pour chaque mode
+        for (int i = 0; i < 5; i++) {
+            r->spsr[i] = 0;
+        }
+    }
     return r;
 }
 
 void registers_destroy(registers r) {
-    /* � compl�ter... */
+    if (r != NULL) {
+        free(r);
+    }
 }
+
 
 uint8_t registers_get_mode(registers r) {
-    /* � compl�ter... */
-    return SVC;
+    return r->current_mode ;
 }
 
+
+//verifier si un mode specifique a un registre SPSR
 static int registers_mode_has_spsr(registers r, uint8_t mode) {
-    /* � compl�ter... */
-    return 1;
+    switch (mode) {
+        case SVC:
+        case FIQ:
+        case ABT:
+        case UND:
+        case IRQ:
+            return 1; // true, le mode a un SPSR associe
+        default:
+            return 0; // false, le mode n'a pas de SPSR associe
+    }
 }
 
 int registers_current_mode_has_spsr(registers r) {
     return registers_mode_has_spsr(r, registers_get_mode(r));
 }
 
+// verifier si la structure des registres est actuellement dans un mode privilegie
+// mode hors USR & SYS
 int registers_in_a_privileged_mode(registers r) {
-    /* � compl�ter... */
-    return 0;
+    if (r != NULL){
+        if (r->current_mode == USR || r->current_mode == SYS){
+            return 0; // false, pas dans un mode privilegie
+        }
+        return 1; // true, dans un mode privilegie
+    }
+    return 0; // false, en cas de pointeur NULL
 }
 
 uint32_t registers_read(registers r, uint8_t reg, uint8_t mode) {
-    uint32_t value = 0;
-    /* � compl�ter... */
-    return value;
+    if(r != NULL){
+        if(reg < 16 && reg >= 0 ){
+            return r->general_registers[reg];
+        }
+    }
+    return UINT32_MAX;  // Valeur speciale indiquant une erreur (definie dans la bib stdint)
+
 }
 
 uint32_t registers_read_cpsr(registers r) {
-    uint32_t value = 0;
-    /* � compl�ter... */
-    return value;
+    if (r != NULL){
+        return r->cpsr;
+    }
+    return UINT32_MAX ;
 }
 
 uint32_t registers_read_spsr(registers r, uint8_t mode) {
-    uint32_t value = 0;
-    /* � compl�ter... */
-    return value;
+    if (r != NULL){
+        if (mode == USR || mode == SYS){
+            return UINT32_MAX;
+        }else {
+            switch (mode){
+                case SVC:
+                    return r->spsr[2];
+                case FIQ:
+                    return r->spsr[0];
+                case ABT:
+                    return r->spsr[3];
+                case UND:
+                    return r->spsr[4];
+                case IRQ:
+                    return r->spsr[1];
+            }
+        }
+    }
+    return UINT32_MAX ;
 }
 
 void registers_write(registers r, uint8_t reg, uint8_t mode, uint32_t value) {
-    /* � compl�ter... */
+
+    if (r != NULL){
+        switch (mode){
+            case USR :
+            case SYS :
+                if(reg < 16 && reg >= 0 ) {
+                    r->general_registers[reg] = value;
+                }
+                break;
+
+            case SVC:
+            case ABT:
+            case UND:
+            case IRQ:
+                if (reg < 16 && reg >= 0) {
+                    r->general_registers[reg] = value;
+                }
+                break;
+
+            case FIQ:
+                if (reg < 8 && reg >= 0) {
+                    r->general_registers[reg] = value;
+                }
+                break;
+        }
+    }
 }
 
 void registers_write_cpsr(registers r, uint32_t value) {
-    /* � compl�ter... */
+    if (r != NULL) {
+        r->cpsr = value;
+        //mettre a jour current_mode ! masquage ...
+    }
 }
 
 void registers_write_spsr(registers r, uint8_t mode, uint32_t value) {
-    /* � compl�ter... */
+    if (r != NULL && registers_mode_has_spsr(r, mode)) {
+        switch (mode) {
+            case FIQ:
+                r->spsr[0] = value;
+                break;
+            case IRQ:
+                r->spsr[1] = value;
+                break;
+            case SVC:
+                r->spsr[2] = value;
+                break;
+            case ABT:
+                r->spsr[3] = value;
+                break;
+            case UND:
+                r->spsr[4] = value;
+                break;
+            default:
+                break;
+        }
+    }
 }
