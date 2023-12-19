1f1f68243b0b15fd3efafdacab6e3afbb0e03b7d
Ismailabdraman <152191207+Ismailabdraman@users.noreply.github.com>
2023-12-19 17:48:37 +0100

    Update arm_data_processing.c addition soustraction

--- arm_simulator-1.5/arm_data_processing.c
+++ arm_simulator-1.5/arm_data_processing.c
@@ -1,24 +1,24 @@
 /*
-Armator - simulateur de jeu d'instruction ARMv5T à but pédagogique
+Armator - simulateur de jeu d'instruction ARMv5T ï¿½ but pï¿½dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique Générale GNU publiée par la Free Software
-Foundation (version 2 ou bien toute autre version ultérieure choisie par vous).
+termes de la Licence Publique Gï¿½nï¿½rale GNU publiï¿½e par la Free Software
+Foundation (version 2 ou bien toute autre version ultï¿½rieure choisie par vous).
 
-Ce programme est distribué car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribuï¿½ car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but spécifique. Reportez-vous à la
-Licence Publique Générale GNU pour plus de détails.
+commercialisation ou d'adaptation dans un but spï¿½cifique. Reportez-vous ï¿½ la
+Licence Publique Gï¿½nï¿½rale GNU pour plus de dï¿½tails.
 
-Vous devez avoir reçu une copie de la Licence Publique Générale GNU en même
-temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software
+Vous devez avoir reï¿½u une copie de la Licence Publique Gï¿½nï¿½rale GNU en mï¿½me
+temps que ce programme ; si ce n'est pas le cas, ï¿½crivez ï¿½ la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
-États-Unis.
+ï¿½tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-	 Bâtiment IMAG
+	 Bï¿½timent IMAG
 	 700 avenue centrale, domaine universitaire
-	 38401 Saint Martin d'Hères
+	 38401 Saint Martin d'Hï¿½res
 */
 #include "arm_data_processing.h"
 #include "arm_exception.h"
@@ -26,9 +26,105 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_branch_other.h"
 #include "util.h"
 #include "debug.h"
+#include "registers.c"
+#include "arm_core.c"
+
+
+
 
 /* Decoding functions for different classes of instructions */
 int arm_data_processing_shift(arm_core p, uint32_t ins) {
+    uint8_t op_code = get_bits(ins, 24, 21);
+    uint8_t set_cond = get_bit(ins, 20);
+    uint8_t rn = get_bits(ins, 19, 16);
+    uint8_t rd = get_bits(ins, 15, 12);
+    //uint8_t rs = get_bits(ins, 11, 8);
+    //uint8_t shift_type = get_bits(ins, 7, 6);
+    //uint8_t shift_imm = get_bits(ins, 11, 7);
+    //uint8_t rm = get_bits(ins, 3, 0);
+
+
+    uint8_t opcode1 = get_bits(ins, 27, 26);
+
+    uint32_t val_rn = p->reg->general_registers[rn];
+    uint32_t val_rd = p->reg->general_registers[rd];
+    
+
+    if (opcode1==0b00){//ADDITION ou soustraction 
+        if(op_code==4){// cas addition 
+            uint32_t shift_op = get_bits(ins, 11, 0);
+            val_rd= val_rn + shift_op;
+            if((set_cond == 1) && (val_rd == p->reg->general_registers[15])){
+                if (registers_current_mode_has_spsr(p->reg)){ //-----------------a demander au prof
+                    p->reg->cpsr = arm_read_spsr(p);
+                }else return UNDEFINED_INSTRUCTION;
+            }
+            else if (set_cond == 1) {
+                uint8_t x = get_bit(val_rd,31);// recuperer N
+                if(x){
+                    p->reg->cpsr = set_bit(p->reg->cpsr, 31);
+                }else{
+                    p->reg->cpsr = clr_bit(p->reg->cpsr,31);
+                }
+
+                if(val_rn==0){ // recuperer Z                           a demander au prof
+                    p->reg->cpsr = set_bit(p->reg->cpsr,30);
+                }else   
+                { p->reg->cpsr = clr_bit(p->reg->cpsr,30);}
+                    uint8_t carry=val_rd< val_rn; //recuperer C
+                if(carry){
+                    p->reg->cpsr = set_bit(p->reg->cpsr,29);
+                }else{
+                    p->reg->cpsr = clr_bit(p->reg->cpsr,29);
+                }
+                
+                uint8_t Vsign = !get_bit(val_rn,31) && get_bit(val_rd,31);  // recuperer V
+                if (Vsign){
+                    p->reg->cpsr = set_bit(p->reg->cpsr,28);
+                }else{
+                    p->reg->cpsr = clr_bit(p->reg->cpsr,28);
+                }
+        }}
+        
+        
+        else if (op_code==2){
+             uint32_t shift_op = get_bits(ins, 11, 0);
+             val_rd= val_rn - shift_op; //opartion de soustraction 
+             if((set_cond == 1) && (val_rd == p->reg->general_registers[15])){
+                if (registers_current_mode_has_spsr(p->reg)){ //-----------------a demander au prof
+                    p->reg->cpsr = arm_read_spsr(p);
+                }else return UNDEFINED_INSTRUCTION;
+            }
+            else if (set_cond == 1) {
+                uint8_t x = get_bit(val_rd,31); //recuperer N
+                if(x){
+                    p->reg->cpsr = set_bit(p->reg->cpsr, 31);
+                }else{
+                    p->reg->cpsr = clr_bit(p->reg->cpsr,31);
+                }
+
+                if(val_rn==0){ //recuperer Z                            a demander au prof
+                    p->reg->cpsr = set_bit(p->reg->cpsr,30);
+                }else   
+                { p->reg->cpsr = clr_bit(p->reg->cpsr,30);}
+                    uint8_t carry=val_rd> val_rn; //reuperer C
+                if(carry){
+                    p->reg->cpsr = set_bit(p->reg->cpsr,29);
+                }else{
+                    p->reg->cpsr = clr_bit(p->reg->cpsr,29);
+                }
+                
+                uint8_t Vsign = get_bit(val_rn,31) && !get_bit(val_rd,31);//recuperer V
+                if (Vsign){
+                    p->reg->cpsr = set_bit(p->reg->cpsr,28);
+                }else{
+                    p->reg->cpsr = clr_bit(p->reg->cpsr,28);
+                }
+        }
+
+
+        }
+    }
     return UNDEFINED_INSTRUCTION;
 }
 
