002086e5638c610274557481c4916a7a357667f9
Mohamed Amine El Ouechrine <150486003+amine-elouechrine@users.noreply.github.com>
2023-12-19 13:55:17 +0100

    finish arm_branch

--- arm_simulator-1.5/arm_branch_other.c
+++ arm_simulator-1.5/arm_branch_other.c
@@ -26,40 +26,42 @@ Contact: Guillaume.Huard@imag.fr
 #include <debug.h>
 #include <stdlib.h>
 
-
 int arm_branch(arm_core p, uint32_t ins) {
     //verification si c'est un branch immediate ou register
     if( get_bits(ins, 27, 25)==5){
         //il s'agit d'une B ou BL
         if(get_bit(ins,24)==1){
             //il s'agit d'un BL
-            p->register[14]=p->register[15];
+            p->reg->general_registers[14]=p->reg->general_registers[15];
          }
         uint32_t offset= (uint32_t )get_bits(ins,23,0);
         if(get_bit(offset,23)==1){
-            offset = set_bits(offset, 31, 24, 0xFF)// adresse a rajouter a pc;
             offset=offset << 2;//multiplier par 4
         }
-        p->register[15]=p->register[15]+offset;//pc<-pc+offset
+        p->reg->general_registers[15]=p->reg->general_registers[15]+offset;//pc<-pc+offset
         return 0;
 
     }
 
     else if (get_bits(ins,27,20)==18){
         //Il s'agit d'un BX ou BLX
+        uint8_t n_registre;
+        uint32_t registre;
         if(get_bits(ins,7,4)==1){
             //c'est un BX
-            uint8_t n_registre= get_bits(ins,3,0);//n de registre code sur 4 bits
-            uint32_t registre= p->reg[n_registre];//recuperer la valeur de ce registre
-            p->register[15]=registre & 0xFFFFFFFE;//Page A4-20
+            n_registre= get_bits(ins,3,0);//n de registre code sur 4 bits
+            registre= p->reg->general_registers[n_registre];//recuperer la valeur de ce registre
+            p->reg->general_registers[15]=registre & 0xFFFFFFFE;//Page A4-20
             return 0;
         }
-        /*else{PAS DE BLX
+        else if (get_bits(ins,7,4)==3){
             //c'est BXL
-            p->register[14]=p->register[15]; //LR<-adress
-            p->register=p->register+get_bits(ins,3,0);
+            n_registre= get_bits(ins,3,0);
+            registre=p->reg->general_registers[n_registre];
+            p->reg->general_registers[14]=p->reg->general_registers[15]; //LR<-pc
+            p->reg->general_registers[15]=registre & 0xFFFFFFFE;
 
-        }*/
+        }
     }
     return 1;
 }
