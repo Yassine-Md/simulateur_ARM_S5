ec85e563e3430a277630ae5ee93773d5fca048bf
yosri20112127 <132912919+yosri20112127@users.noreply.github.com>
2023-12-20 16:00:24 +0100

    Update arm_load_store.c

--- arm_simulator-1.5/arm_load_store.c
+++ arm_simulator-1.5/arm_load_store.c
@@ -80,9 +80,13 @@ uint32_t arm_read_register(arm_core p, uint8_t reg) // return Rreg value
 /*
 if (load_store == 0){ // Store
     if(B == 0){ // Word
-                                    
+    	uint32_t val=arm_read_register(p,Rn)
+     	uint32_t address=arm_read_register(p,Rd);
+        arm_write_word(p,address,val);                  
     }else{ //Byte
-            
+        uint32_t val=arm_read_register(p,Rn)
+     	uint32_t address=arm_read_register(p,Rd);
+        arm_write_byte(p,address,(uint8_t)val);
     }
     
 }else{ // Load
