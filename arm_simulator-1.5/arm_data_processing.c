60b7dd68168cd693a0d10edb2d63db0667b3e275
Mohamed Amine El Ouechrine <150486003+amine-elouechrine@users.noreply.github.com>
2023-12-27 17:26:26 +0100

    Update arm_data_processing.c

--- arm_simulator-1.5/arm_data_processing.c
+++ arm_simulator-1.5/arm_data_processing.c
@@ -1,25 +1,25 @@
 
 /*
-Armator - simulateur de jeu d'instruction ARMv5T   but p dagogique
+Armator - simulateur de jeu d'instruction ARMv5T � but p�dagogique
 Copyright (C) 2011 Guillaume Huard
 Ce programme est libre, vous pouvez le redistribuer et/ou le modifier selon les
-termes de la Licence Publique G n rale GNU publi e par la Free Software
-Foundation (version 2 ou bien toute autre version ult rieure choisie par vous).
+termes de la Licence Publique G�n�rale GNU publi�e par la Free Software
+Foundation (version 2 ou bien toute autre version ult�rieure choisie par vous).
 
-Ce programme est distribu  car potentiellement utile, mais SANS AUCUNE
+Ce programme est distribu� car potentiellement utile, mais SANS AUCUNE
 GARANTIE, ni explicite ni implicite, y compris les garanties de
-commercialisation ou d'adaptation dans un but sp cifique. Reportez-vous   la
-Licence Publique G n rale GNU pour plus de d tails.
+commercialisation ou d'adaptation dans un but sp�cifique. Reportez-vous � la
+Licence Publique G�n�rale GNU pour plus de d�tails.
 
-Vous devez avoir re u une copie de la Licence Publique G n rale GNU en m me
-temps que ce programme ; si ce n'est pas le cas,  crivez   la Free Software
+Vous devez avoir re�u une copie de la Licence Publique G�n�rale GNU en m�me
+temps que ce programme ; si ce n'est pas le cas, �crivez � la Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
- tats-Unis.
+�tats-Unis.
 
 Contact: Guillaume.Huard@imag.fr
-     B timent IMAG
+     B�timent IMAG
      700 avenue centrale, domaine universitaire
-     38401 Saint Martin d'H res
+     38401 Saint Martin d'H�res
 */
 #include "arm_data_processing.h"
 #include "arm_exception.h"
